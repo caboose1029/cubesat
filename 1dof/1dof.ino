2daa4a516bcfd1fcb39a7a2c6e51ec74bac37606
Ryan Taylor <taylor48@my.erau.edu>
2022-09-08 09:56:40 -0400

    First serious attempt at a PID controller

--- 1dof/1dof.ino
+++ 1dof/1dof.ino
@@ -1,13 +1,44 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
-#define PWM     9
+#include "QuickPID.h"   //PID https://github.com/Dlloydev/QuickPID/blob/master/examples/PID_Basic/PID_Basic.ino
 Adafruit_MPU6050 mpu;
 
-void setup(void) {
-  
+// define pins for Nidec 24H Motor
+#define DIRECTION   7 
+#define BRAKE       8
+#define PWM         9
 
+// Set variables for acceleration in y and z; input and output; bounds; PID
+double accY, accZ;
+double thetaIn;
+double thetaOut;
+const double bounds = 0.5;
+double pwm = 400;
+int dir = 0;
+
+// Set variables for PID
+float Setpoint, Input, Output;
+float Kp = 10, Ki = 0.5, Kd = 0;
+
+//Specify PID links
+QuickPID myPID(&Input, &Output, &Setpoint);
+
+
+void setup(){
   
+  // Set pin modes & set initial values
+  pinMode(DIRECTION, OUTPUT);
+  pinMode(BRAKE, OUTPUT);
+  pinMode(PWM, OUTPUT);
+  digitalWrite(DIRECTION, LOW);
+  digitalWrite(BRAKE, HIGH);
+  analogWrite(PWM, 400);
+
+  // Set PWM frequency to 20000 hz
+  /*****************************/
+
+  // Begin communication with MPU6050
   Serial.begin(115200);
   while (!Serial) {
     delay(10); // Pause to open console
@@ -30,34 +61,64 @@ void setup(void) {
 
   Serial.println("");
   delay(100);
+
+  // PID Setup
+  //initialize the variables we're linked to
+  Input = thetaIn;
+  Setpoint = 0;
+
+  //Set PID output limits to +/- 400
+  myPID.SetOutputLimits(0, 400);
+
+  //apply PID gains
+  myPID.SetTunings(Kp, Ki, Kd);
+
+  //turn the PID on
+  myPID.SetMode(myPID.Control::automatic);
 }
 
+void loop(){
 
-
-void loop() {
-
-  /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
+  sensor_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
+  thetaIn = atan2(accY,accZ);
 
-  /* Print out the values */
-  Serial.print("AccelX:");
-  Serial.print(a.acceleration.x);
-  Serial.print(",");
-  Serial.print("AccelY:");
-  Serial.print(a.acceleration.y);
-  Serial.print(", ");
-  Serial.print("GyroX:");
-  Serial.print(g.gyro.x);
-  Serial.print(",");
-  Serial.print("GyroY:");
-  Serial.print(g.gyro.y);
-  Serial.println("");
+  if (thetaIn > -bounds && thetaIn < bounds){
+    
+    pwm = 400;
 
-  if (a.acceleration.x <= 0) {
-    digitalWrite(PWM, LOW);
-    } else{
-    digitalWrite(PWM, HIGH);
   }
-  delay(10);
+
+  float delta = Setpoint - thetaIn;
+  
+  if (delta > 0){
+    dir = 0;
   }
+  else{
+    dir = 1;
+  }
+
+  Input = thetaIn;
+  myPID.Compute();
+  pwm = map(Output, 0, 400, 400, 0);
+
+  driveMotor(pwm, dir);
+  Serial.println(pwm);
+  
+}
+
+void driveMotor(float pwm, int dir){
+  if(dir == 0){
+
+    digitalWrite(DIRECTION, LOW);
+    analogWrite(PWM, pwm);
+
+  }
+
+  else if(dir == 1){
+
+    digitalWrite(DIRECTION, HIGH);
+    analogWrite(PWM, pwm);
+
+  }
+}
